883a4b82af57b08ef8f8f3eff8d08486532d4dde
William Hardy Gest <william.gest@q2ebanking.com>
2014-07-16 11:07:29 -0500

    in major trouble now

--- SMS_Server.py
+++ SMS_Server.py
@@ -4,14 +4,6 @@ import os
 import json
 import re
 import shutil
-import smtplib
-import os
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email.mime.text import MIMEText
-from email.utils import COMMASPACE, formatdate
-from email import Encoders
-
 
 from flask import Flask, request, make_response
 
@@ -42,25 +34,11 @@ def add_entrant(initials, number):
             json.dump(entrants, fout)
         print "added", number
         shutil.copy("entrants.txt", "entrantsbackup.txt")
-        try:
-            send_email()
-        except:
-            pass
         return True, None
     except:
         print "dump failure"
         return False, "dump"
 
-def make_fixtures():
-    try:
-        with open(DATABASE, "r") as fin:
-            entrants = json.load(fin)
-        entrants["+15124843205"] = "WHG"
-        with open(DATABASE, "w") as fout:
-            json.dump(entrants, fout)
-        return True
-    except:
-        return False
 
 def notify_winner(winner):
     try:
@@ -71,7 +49,7 @@ def notify_winner(winner):
             json.dump(entrants, fout)
     except:
         pass
-    body = "CONGRATULATIONS! The wheel o' winners has chosen you! Show this text to the MC to claim your prize!"
+    body = "CONGRATULATIONS! The Q2 wheel o' winners has chosen you! Show this text to claim your prize!"
     message = client.messages.create(body=body,
                                      to=winner,
                                      from_="+15129107535")
@@ -85,37 +63,8 @@ def notify_database_error(number, err):
     print message.sid
 
 def clear_db():
-
     with open(DATABASE, 'w') as fout:
         fout.write('{}')
-
-def send_email():
-    send_from = "ERAFFLE"
-    send_to = "william.gest@q2ebanking.com"
-    subj = "entrants"
-
-    msg = MIMEMultipart()
-    msg['From'] = send_from
-    msg['To'] = COMMASPACE.join(send_to)
-    msg['Date'] = formatdate(localtime=True)
-    msg['Subject'] = "REVIEW SCRAPER REPORT: " + subj
-
-
-    part = MIMEBase('application', "octet-stream")
-    part.set_payload(open("entrants.txt", "rb").read())
-    Encoders.encode_base64(part)
-    part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename("entrants.txt"))
-    msg.attach(part)
-    server = smtplib.SMTP('smtp.gmail.com:587')
-    server.ehlo()
-    server.starttls()
-    server.login("q2scraper@gmail.com", "scraper!!")
-
-    try:
-        server.sendmail(send_from, send_to, msg.as_string())
-    except:
-        pass
-    server.close()
 
 @app.route('/', methods=['GET', 'POST'])
 def receive_text():
